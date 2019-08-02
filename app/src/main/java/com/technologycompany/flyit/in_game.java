40b389c2f0f673c9ab22e1d455ac75f2c0c745eb
Hussnain Jaffri <hajaffri33@gmail.com>
2019-08-02 01:21:00 -0700

    Views: Home button Pause button

---
+++ app/src/main/java/com/technologycompany/flyit/in_game.java
@@ -0,0 +1,14 @@
+package com.technologycompany.flyit;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class in_game extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_in_game);
+    }
+}
