1b1bfe1209d35fe92c3b50e2cab88891eaac7ed0
Hussnain Jaffri <hajaffri33@gmail.com>
2019-10-09 23:00:51 -0700

    Database

--- app/src/main/java/com/technologycompany/flyornot/in_game.java
+++ app/src/main/java/com/technologycompany/flyornot/in_game.java
@@ -2,18 +2,34 @@ package com.technologycompany.flyornot;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.GestureDetector;
 import android.view.MotionEvent;
+import android.widget.ImageView;
 import android.widget.ProgressBar;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.bumptech.glide.Glide;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class in_game extends AppCompatActivity implements GestureDetector.OnGestureListener {
 
-    ProgressBar pb;
-    int counter = 0;
+    FirebaseDatabase firebaseDatabase = FirebaseDatabase.getInstance();
+    DatabaseReference databaseReference = firebaseDatabase.getReference();
+    DatabaseReference imageRef = databaseReference.child("images");
+
+
+    int  id = 1, counter = 0;
     private int best_score = 0;
-    TextView score;
+    String name, url;
+    TextView score,textName;
+    ImageView imageView;
 
     private GestureDetector gestureDetector;
 
@@ -23,23 +39,15 @@ public class in_game extends AppCompatActivity implements GestureDetector.OnGest
         setContentView(R.layout.activity_in_game);
 
         gestureDetector = new GestureDetector(this,this);
-
         score = findViewById(R.id.score);
+        textName  = findViewById(R.id.name);
+        imageView = findViewById(R.id.image_viewer);
+
+       loadimage();
 
 
-      /*fly.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                fly();
-            }
-        });
 
-        cant_fly.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                cant_fly();
-            }
-        }); */
+
     }
 
 
@@ -82,18 +90,63 @@ public class in_game extends AppCompatActivity implements GestureDetector.OnGest
 
         if(e1.getY() - e2.getY() > 50){
             score.setText("UP");
+            id++;
+            loadimage();
+            check_fly();
             return true;
         }
 
         if(e2.getY() - e1.getY() > 50){
             score.setText("DOWN");
+            id--;
+            loadimage();
+            check_fly();
             return true;
         }
 
         return false;
     }
-
 //    Gestures Ended
+
+
+//    Checking if fly or not
+    public void check_fly(){
+
+
+
+
+    }
+//    Checking Ended
+
+
+
+//    Retrieving the images/names
+
+    public void loadimage(){
+
+        imageRef.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                // This method is called once with the initial value and again
+                // whenever data at this location is updated.
+                String ids = String.valueOf(id);
+                name = dataSnapshot.child(ids).child("name").getValue(String.class);
+                url  = dataSnapshot.child(ids).child("url").getValue(String.class);
+                textName.setText(name);
+                Glide.with(in_game.this).load(url).into(imageView);
+
+            }
+
+            @Override
+            public void onCancelled(DatabaseError error) {
+                // Failed to read value
+
+            }
+        });
+
+
+
+    }
 
 
 
@@ -111,15 +164,6 @@ public class in_game extends AppCompatActivity implements GestureDetector.OnGest
        };
        t.schedule(tt, 0 , 30);
     }
-
-    public void fly(){
-           prog();
-    }
-
-    public void cant_fly(){
-        Intent intent = new Intent(this, MainActivity.class);
-        startActivity(intent);
-        finish();
-    }*/
+*/
 
 }
